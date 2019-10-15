b97852b2e1944a57f064f4ee7008c0df74d5a96b
ilyatim <31735653+ilyatim@users.noreply.github.com>
2019-10-15 11:54:20 +0300

    update course work
    - replaced icon (plus, minus)
    - change icon size
    - refactor name of drawable file
    - move bindingAdapter and create new method

---
+++ TestCourseWork/app/src/main/java/com/example/testcoursework/ui/BindingAdapters.kt
@@ -0,0 +1,25 @@
+package com.example.testcoursework.ui
+
+import android.databinding.BindingAdapter
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import com.example.testcoursework.data.Singleton
+
+object BindingAdapters
+{
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @BindingAdapter("setClipToOutLine")
+    @JvmStatic fun setClipToOutLine(imgBtn: ImageButton, boolean: Boolean)
+    {
+        imgBtn.clipToOutline = boolean
+    }
+
+    fun onClickIncrease()
+    {
+        Log.d("increase", "da")
+        Singleton.personActivity.increaseWater()
+    }
+}
